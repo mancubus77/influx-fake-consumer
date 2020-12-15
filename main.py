a6b63d334d6b731f9fb82bfa073f9898dea24df4
Stan Kozlov <stankozlov@Stans-MacBook-Pro.local>
2020-12-15 23:53:34 +1100

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from confluent_kafka import Consumer, KafkaError
+from influxdb import InfluxDBClient
+
+TOPIC = "telemetry"
+
+settings = {
+    "bootstrap.servers": "my-cluster-kafka-brokers:9092",
+    "group.id": "mygroup",
+    "client.id": "client-1",
+    "enable.auto.commit": True,
+    "session.timeout.ms": 6000,
+    "default.topic.config": {"auto.offset.reset": "smallest"},
+}
+
+c = Consumer(settings)
+
+c.subscribe([TOPIC])
+client = InfluxDBClient(
+    "http://vminsert-example-vmcluster-persistent", 8480, path="/insert/0/influx/"
+)
+
+
+try:
+    while True:
+        msg = c.poll(0.1)
+        if msg is None:
+            continue
+        elif not msg.error():
+            print("Received message: {0}".format(msg.value()))
+            client.write_points([msg.value()])
+        elif msg.error().code() == KafkaError._PARTITION_EOF:
+            print(
+                "End of partition reached {0}/{1}".format(msg.topic(), msg.partition())
+            )
+        else:
+            print("Error occured: {0}".format(msg.error().str()))
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    c.close()
